a767deb7f040ec86587a8b73cdde095656196c8e
varnao <valeria2023arnao@gmail.com>
2025-10-16 01:14:55 -0700

    feat: add new blog files, dynamically create and append blog elements

---
+++ src/blog.js
@@ -0,0 +1,41 @@
+var blogs = [
+  {
+    title: "Why I Love Frontend Development",
+    date: "October 15, 2025",
+    description:
+      "Discussing why frontend development is my favorite part of programming, from bringing designs to life to creating experiences that feel intuitive and engaging.",
+    image: "./images/frontend.jpeg",
+    imageAlt: "photo of a computer screen",
+    slug: "",
+  },
+  {
+    title: "My Involvement In WISH at Cal Poly",
+    date: "October 15, 2025",
+    description:
+      "A look into my involvement with WISH at Cal Poly and how the club has strengthened my technical skills, leadership, and sense of community in tech.",
+    image: "./images/wishlogo.png",
+    imageAlt: "cal poly wish club logo",
+    slug: "",
+  },
+];
+var blogContainer = document.getElementById("blog-container");
+if (blogContainer) {
+  blogs.forEach(function (blog) {
+    var image = document.createElement("img");
+    image.src = blog.image;
+    image.alt = blog.imageAlt;
+    var title = document.createElement("h1");
+    title.textContent = blog.title;
+    var date = document.createElement("p");
+    date.textContent = blog.date;
+    var description = document.createElement("p");
+    description.textContent = blog.description;
+    var blogCard = document.createElement("div");
+    blogCard.classList.add("blog-card");
+    blogCard.appendChild(image);
+    blogCard.appendChild(title);
+    blogCard.appendChild(date);
+    blogCard.appendChild(description);
+    blogContainer.appendChild(blogCard);
+  });
+}
---
+++ src/blog.ts
@@ -0,0 +1,57 @@
+type Blog = {
+  title: string;
+  date: string;
+  description: string;
+  image: string;
+  imageAlt: string;
+  slug: string;
+};
+
+const blogs: Blog[] = [
+  {
+    title: "Why I Love Frontend Development",
+    date: "October 15, 2025",
+    description:
+      "Discussing why frontend development is my favorite part of programming, from bringing designs to life to creating experiences that feel intuitive and engaging.",
+    image: "./images/frontend.jpeg",
+    imageAlt: "photo of a computer screen",
+    slug: "",
+  },
+  {
+    title: "My Involvement In WISH at Cal Poly",
+    date: "October 15, 2025",
+    description:
+      "A look into my involvement with WISH at Cal Poly and how the club has strengthened my technical skills, leadership, and sense of community in tech.",
+    image: "./images/wishlogo.png",
+    imageAlt: "cal poly wish club logo",
+    slug: "",
+  },
+];
+
+const blogContainer = document.getElementById("blog-container");
+
+if (blogContainer) {
+  blogs.forEach((blog) => {
+    const image = document.createElement("img");
+    image.src = blog.image;
+    image.alt = blog.imageAlt;
+
+    const title = document.createElement("h1");
+    title.textContent = blog.title;
+
+    const date = document.createElement("p");
+    date.textContent = blog.date;
+
+    const description = document.createElement("p");
+    description.textContent = blog.description;
+
+    const blogCard = document.createElement("div");
+    blogCard.classList.add("blog-card");
+
+    blogCard.appendChild(image);
+    blogCard.appendChild(title);
+    blogCard.appendChild(date);
+    blogCard.appendChild(description);
+    blogContainer.appendChild(blogCard);
+  });
+}
